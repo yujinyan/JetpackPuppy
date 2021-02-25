4b47c22e9c78410e246a843948f690022379b3e7
Yu Jinyan <yujinyan@outlook.com>
2021-02-25 17:44:52 +0800

    Wildcard import

--- app/src/main/java/com/example/androiddevchallenge/DetailActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/DetailActivity.kt
@@ -6,7 +6,11 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
